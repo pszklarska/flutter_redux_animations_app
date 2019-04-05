b7fef2c1c405712759ee7a70d37a4defcde4747a
pszklarska <pszklarska@gmail.com>
2019-04-05 09:40:11 +0700

    Adding cart value bar animation

--- lib/list/cart_value_bar.dart
+++ lib/list/cart_value_bar.dart
@@ -2,31 +2,85 @@ import 'package:flutter/material.dart';
 
 const CART_BAR_HEIGHT = 48.0;
 
-class CartValueBar extends StatelessWidget {
+class CartValueBar extends StatefulWidget {
   final double cartValue;
 
   const CartValueBar({Key key, this.cartValue})
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return cartValue != 0
-        ? Container(
-            height: CART_BAR_HEIGHT,
-            color: Colors.orange,
-            child: Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Cart value ${_getCartValue()} zł',
-                  ),
-                ),
-              ],
-            ),
-          )
-        : Container();
+  _CartValueBarState createState() => _CartValueBarState();
+}
+
+class _CartValueBarState extends State<CartValueBar>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    )..addListener(() {
+      setState(() {});
+    });
+
+    _animation = Tween<double>(
+      begin: CART_BAR_HEIGHT,
+      end: 0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOut,
+        reverseCurve: Curves.easeIn,
+      ),
+    );
   }
 
-  String _getCartValue() => cartValue.toStringAsFixed(2);
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, _animation.value),
+      child: Container(
+        height: CART_BAR_HEIGHT,
+        color: Colors.orange,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0),
+              child: Text(
+                'Cart value ${_getCartValue()} zł',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void didUpdateWidget(CartValueBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.cartValue == 0) {
+      _controller.forward();
+    }
+
+    if (widget.cartValue == 0) {
+      _controller.reverse();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  String _getCartValue() =>
+      widget.cartValue.toStringAsFixed(2);
 }
